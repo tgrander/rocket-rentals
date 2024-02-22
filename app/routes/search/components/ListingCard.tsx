0a45b082d84a60f7090040486a3cad91908958a9
Trey Granderson <treygranderson@gmail.com>
2024-02-22 06:02:01 -0500

    grid of listing cards

--- app/routes/search/components/ListingCard.tsx
+++ app/routes/search/components/ListingCard.tsx
@@ -11,10 +11,10 @@ interface Props {
   rating: string;
 }
 
-export const ListingCard: React.FC<Props> = () => {
+export const ListingCard: React.FC = () => {
   return (
-    <CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+    <CardContainer className="inter-var" containerClassName="py-0">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-full  h-auto rounded-xl p-6 border  ">
         <CardItem translateZ="100" className="w-full mb-12">
           <img
             src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
--- app/routes/search/route.tsx
+++ app/routes/search/route.tsx
@@ -5,8 +5,12 @@ import { ListingCard } from "./components";
 export default function SearchPage() {
   usePageStyles("h-full bg-gray-900");
   return (
-    <div className="w-screen flex justify-center py-14">
-      <ListingCard />
+    <div className="w-screen px-10 py-14">
+      <div className="grid grid-cols-3 gap-4">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <ListingCard key={i} />
+        ))}
+      </div>
     </div>
   );
 }
