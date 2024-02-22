333e33d5504855abd5daee66525e2c04310057e0
Trey Granderson <treygranderson@gmail.com>
2024-02-22 08:23:05 -0500

    tailwind config: aspect-ratio plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,10 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
@@ -6,5 +12,15 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio"), addVariablesForColors],
 } satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+  addBase({
+    ":root": newVars,
+  });
+}
