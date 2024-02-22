b12379cb0866936ec3cfdef178a763b32f2d5809
Trey Granderson <treygranderson@gmail.com>
2024-02-22 04:13:40 -0500

    search: render listing card

--- app/routes/search/route.tsx
+++ app/routes/search/route.tsx
@@ -1,11 +1,12 @@
 import { usePageStyles } from "~/hooks";
 import { cn } from "~/utils";
+import { ListingCard } from "./components";
 
-export default function Example() {
+export default function SearchPage() {
   usePageStyles("h-full bg-gray-900");
   return (
-    <>
-      <div></div>
-    </>
+    <div className="w-screen flex justify-center py-14">
+      <ListingCard />
+    </div>
   );
 }
