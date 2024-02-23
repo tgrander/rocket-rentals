041da2170d92cbe30c74a20cce63e2f1051c64db
Trey Granderson <treygranderson@gmail.com>
2024-02-23 02:02:40 -0500

    cloudinary img url

--- app/routes/search/components/ListingCard.tsx
+++ app/routes/search/components/ListingCard.tsx
@@ -13,17 +13,19 @@ interface Props {
 
 export const ListingCard: React.FC = () => {
   return (
-    <CardContainer className="inter-var z-50" containerClassName="py-0">
+    <CardContainer className="inter-var" containerClassName="py-0">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full  h-auto rounded-xl p-6 border">
-        {/* Image */}
+        {/* IMAGE */}
         <CardItem translateZ="100" className="w-full mb-12">
           <img
-            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            src="https://res.cloudinary.com/dfjcoifwd/image/upload/w_30,h_30,c_scale,e_blur:1000,f_auto/v1/spacecrafts/l8vqrdp6aoohwozb0b4i"
+            data-src="https://res.cloudinary.com/dfjcoifwd/image/upload/f_auto,q_80,dpr_auto,w_700,h_648,c_fill/fl_progressive/v1/spacecrafts/l8vqrdp6aoohwozb0b4i"
+            className="lazyload h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
             alt="thumbnail"
           />
         </CardItem>
-        {/* Type */}
+
+        {/* TYPE */}
         <CardItem
           as="p"
           translateZ="40"
@@ -31,7 +33,8 @@ export const ListingCard: React.FC = () => {
         >
           Spaceplane
         </CardItem>
-        {/* Title */}
+
+        {/* TITLE */}
         <CardItem
           translateZ="60"
           className="font-bold text-neutral-600 dark:text-white text-2xl"
@@ -39,14 +42,15 @@ export const ListingCard: React.FC = () => {
           Rocket Sparrow X11
         </CardItem>
         <div className="flex justify-between items-center mt-10">
-          {/* Price */}
+          {/* PRICE */}
           <CardItem
             translateZ={50}
             className="py-2 rounded-xl text-xl font-normal dark:text-white"
           >
             ⧫100,000 day
           </CardItem>
-          {/* Rating */}
+
+          {/* RATING */}
           <CardItem
             translateZ={20}
             className="flex items-center space-x-1 px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
--- app/routes/search/route.tsx
+++ app/routes/search/route.tsx
@@ -24,7 +24,7 @@ export default function SearchPage() {
           <ListingCard key={i} />
         ))}
       </div>
-      <BackgroundBeams />
+      {/* <BackgroundBeams /> */}
     </div>
   );
 }
